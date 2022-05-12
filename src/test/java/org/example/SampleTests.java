2e8db725e829702643fce6ed32945036bdb0da9c
Rony Byalsky <rony.byalsky@gmail.com>
2022-05-12 18:47:49 +0300

    Added some more tests

--- src/test/java/org/example/SampleTests.java
+++ src/test/java/org/example/SampleTests.java
@@ -3,6 +3,7 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -13,7 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
-public class AmazonTests {
+public class SampleTests {
 
     private String baseUrl = null;
     private List<String> searchTerms = null;
@@ -27,13 +28,14 @@ public class AmazonTests {
     }
 
     @Test
-    public void amazonSearchTest() {
+    public void seleniumAmazonSearchTest() {
         System.setProperty("webdriver.chrome.driver", "webdriver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         for (String searchTerm : searchTerms) {
+            Reporter.log("Navigate to: " + baseUrl, true);
             driver.get(baseUrl);
             driver.findElement(By.id("twotabsearchtextbox")).sendKeys(searchTerm);
             driver.findElement(By.id("nav-search-submit-button")).click();
@@ -42,5 +44,28 @@ public class AmazonTests {
         }
 
         driver.quit();
+    }
+
+    @Test
+    public void flakyTest() {
+        Reporter.log("Generating a random int in range: 0-100", true);
+        Reporter.log("Test will fail if the int is odd", true);
+        int random = randomInt(0, 100);
+        Assert.assertEquals(random % 2, 0, "Random int is: " + random + " - that's an odd number :(");
+        Reporter.log("Random int is: " + random + " - that's an even number - Great!", true);
+    }
+
+    @Test
+    public void passTest() {
+        Reporter.log("Hello world! Everything's OK :)");
+    }
+
+    @Test
+    public void failTest() {
+        Assert.fail("This test is doomed to always fail :( ");
+    }
+
+    private int randomInt(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
     }
 }
